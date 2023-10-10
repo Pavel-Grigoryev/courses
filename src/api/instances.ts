9428a0bab03a2e382d2feacb830a23926b931007
Pavel <3935371@gmail.com>
2023-10-10 21:27:42 +0300

    fix(instance): changed base url-2

--- src/api/instances.ts
+++ src/api/instances.ts
@@ -3,5 +3,5 @@ import axios from 'axios';
 export const instance = axios.create({
     baseURL: process.env.NODE_ENV === 'development'
         ? 'http://localhost:3000/api'
-        : 'https://courses-git-main-pavel-grigoryev.vercel.app/api',
+        : '/api',
 });
